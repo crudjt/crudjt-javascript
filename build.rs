ec7a1c04637f0728b0abd112ddfcd50d51ab3eb6
v_akymov <vladyslav.akimov@ralabs.org>
2024-11-19 11:13:40 +0200

    added DEMO CI/CD

--- build.rs
+++ build.rs
@@ -1,3 +1,39 @@
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
 fn main() {
-    println!(r"cargo:rustc-link-search=/Users/macintosh/Applications/crud_jt-libs/javascript/my-project/native/");
+    // let manifest_dir = env::var("CARGO_MANIFEST_DIR").expect("Failed to get CARGO_MANIFEST_DIR");
+    //
+    // let target_os = env::var("CARGO_CFG_TARGET_OS").expect("Failed to get target OS");
+    // let target_arch = env::var("CARGO_CFG_TARGET_ARCH").expect("Failed to get target architecture");
+    //
+    // let lib_path: PathBuf = match (target_os.as_str(), target_arch.as_str()) {
+    //     ("macos", "x86_64") => PathBuf::from(&manifest_dir).join("native"),
+    //     ("macos", "aarch64") => PathBuf::from(&manifest_dir).join("path/to/macos/aarch64"),
+    //     ("windows", "x86_64") => PathBuf::from(&manifest_dir).join("path/to/windows/x86_64"),
+    //     ("linux", "x86_64") => PathBuf::from(&manifest_dir).join("path/to/linux/x86_64"),
+    //     _ => panic!("Unsupported platform"),
+    // };
+    //
+    // println!("cargo:rustc-link-search={}", lib_path.display());
+    // // println!("cargo:rustc-link-lib=store_jt");
+    //
+    // // println!(r"cargo:rustc-link-search={}", manifest_dir);
+
+    // println!("cargo:rustc-link-search=./native");
+
+
+    // Отримуємо вихідну директорію для збирання
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
+    // Шлях до оригінальної dylib бібліотеки
+    let dylib_path = PathBuf::from("native/libstore_jt.dylib");
+
+    // // Копіюємо dylib бібліотеку в вихідну директорію
+    // fs::copy(&dylib_path, out_dir.join("libstore_jt.dylib"))
+    //     .expect("Failed to copy dylib file");
+
+    // Вказуємо шлях для лінкування бібліотеки
+    println!("cargo:rustc-link-search=native={}", out_dir.display());
 }
--- index.js
+++ index.js
@@ -1,10 +1,129 @@
+module.exports = require('./index.node');
+
+const fs = require('fs');
+// const path = require('path');
+const { promisify } = require('util');
+const sleep = promisify(setTimeout);
+
+
+const os = require('os');
+const path = require('path');
+
+// Визначаємо ОС та архітектуру
+const platform = os.platform(); // 'darwin', 'linux', 'win32'
+const arch = os.arch(); // 'x64', 'arm64', 'ia32', тощо
+
+function copyFileWithCheck(srcPath, destPath) {
+  try {
+    // Перевіряємо існування файлу джерела
+    if (!fs.existsSync(srcPath)) {
+      console.log(`Файл не знайдено: ${srcPath}`);
+      return;
+    }
+
+    // Отримуємо назву файлу та шлях до файлу призначення
+    const fileName = path.basename(srcPath);
+    const destFilePath = path.join(destPath, fileName);
+
+    // Перевіряємо існування файлу призначення
+    if (fs.existsSync(destFilePath)) {
+      // Отримуємо інформацію про файли
+      const srcStats = fs.statSync(srcPath);
+      const destStats = fs.statSync(destFilePath);
+
+      // Якщо назва і розмір збігаються, виходимо з функції
+      if (srcStats.size === destStats.size) {
+        console.log(`Файл вже існує з такою ж назвою та розміром: ${destFilePath}`);
+        return;
+      }
+    }
+
+    // Копіюємо файл
+    if (!fs.existsSync(destPath)) {
+      fs.copyFileSync(sourcePath, targetPath);
+      console.log(`Файл успішно скопійовано в: ${destFilePath}`);
+    }
+
+  } catch (error) {
+    console.error(`Помилка: ${error.message}`);
+  }
+}
+
+///
+// Визначаємо sourcePath
+function getSourcePath() {
+  let archDir;
+  let platformDir;
+
+  // Визначаємо архітектуру
+  switch (arch) {
+    case 'x64':
+      archDir = 'x86_64';
+      break;
+    case 'arm64':
+      archDir = 'arm64';
+      break;
+    default:
+      throw new Error(`Unsupported architecture: ${arch}`);
+  }
+
+  // Визначаємо директорію для платформи
+  switch (platform) {
+    case 'darwin':
+      platformDir = 'macos';
+      break;
+    case 'linux':
+      platformDir = 'linux';
+      break;
+    case 'win32':
+      platformDir = 'windows';
+      break;
+    default:
+      throw new Error(`Unsupported platform: ${platform}`);
+  }
+
+  // Формуємо шлях до бібліотеки
+  return path.join(__dirname, 'native', platformDir, archDir, 'libstore_jt.dylib');
+}
+
+// Визначаємо targetPath
+function getTargetPath() {
+  let systemLibDir;
+  let libName;
+
+  switch (platform) {
+    case 'darwin':
+      systemLibDir = '/usr/local/lib';
+      libName = 'libstore_jt.dylib';
+      break;
+    case 'linux':
+      systemLibDir = '/usr/lib';
+      libName = 'libstore_jt.so';
+      break;
+    case 'win32':
+      systemLibDir = 'C:\\Windows\\System32';
+      libName = 'libstore_jt.dll';
+      break;
+    default:
+      throw new Error(`Unsupported platform: ${platform}`);
+  }
+
+  return path.join(systemLibDir, libName);
+}
+///
+
+const sourcePath = getSourcePath();
+const targetPath = getTargetPath();
+
+copyFileWithCheck(sourcePath, targetPath);
+
 const native = require('.'); // Require the compiled native module
 const msgpack = require('msgpack-lite');
 const { Buffer } = require('buffer');
 const LRUCache = require('./Cache');
 const Validation = require('./Validation');
 
-const lruLRUCache = new LRUCache((value) => native.read(value));
+const lruCache = new LRUCache((value) => native.read(value));
 
 function create(hash, ttl = -1, silence_read = -1) {
     Validation.validateInsertion(hash, ttl, silence_read);
@@ -14,7 +133,7 @@ function create(hash, ttl = -1, silence_read = -1) {
 
     // Call native function create, passing it a pointer and the size of the data
     let token = native.create(packedData, packedData.length, ttl, silence_read);
-    lruLRUCache.insert(token, hash, ttl, silence_read);
+    lruCache.insert(token, hash, ttl, silence_read);
 
     return token;
 }
@@ -22,7 +141,7 @@ function create(hash, ttl = -1, silence_read = -1) {
 function read(token) {
   Validation.validateToken(token);
 
-  let output = lruLRUCache.get(token);
+  let output = lruCache.get(token);
   if (output) {
     return output
   }
@@ -33,7 +152,7 @@ function read(token) {
   }
 
   let result = JSON.parse(result_str);
-  lruLRUCache.forceInsert(token, result);
+  lruCache.forceInsert(token, result);
 
   return result;
 }
@@ -48,7 +167,7 @@ function update(token, hash, ttl = -1, silence_read = -1) {
   // Call native function update, passing it a pointer and the size of the data
   let result = native.update(token, packedData, packedData.length, ttl, silence_read);
   if (result) {
-    lruLRUCache.insert(token, hash, ttl, silence_read)
+    lruCache.insert(token, hash, ttl, silence_read)
   }
 
   return result;
@@ -57,7 +176,7 @@ function update(token, hash, ttl = -1, silence_read = -1) {
 function __delete(token) {
   Validation.validateToken(token);
 
-  lruLRUCache.delete(token);
+  lruCache.delete(token);
 
   return native.delete(token);
 }
