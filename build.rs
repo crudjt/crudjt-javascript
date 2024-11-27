2e81d4e9764bce27b3658f4b42e484eec868993a
v_akymov <vladyslav.akimov@ralabs.org>
2024-11-27 20:06:20 +0200

    CI/CD for Windows x86_64

--- build.rs
+++ build.rs
@@ -72,7 +72,9 @@ fn main() {
     // }
 
     // let dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let system32_dir = env::var("SYSTEM32_DIR").unwrap_or_else(|_| String::from("C:\\Windows\\System32"));
-    println!("cargo:rustc-link-search=native={}", system32_dir);
-    println!("cargo:rustc-link-lib=dylib=store_jt");
+
+
+    // let system32_dir = env::var("SYSTEM32_DIR").unwrap_or_else(|_| String::from("C:\\Windows\\System32"));
+    // println!("cargo:rustc-link-search=native={}", system32_dir);
+    // println!("cargo:rustc-link-lib=dylib=store_jt");
 }
